9a8f331c1984d9f10d78af5488724aa446dc17aa
Ainsley Pilongo <nmk21kfu@UEA.AC.UK>
2024-06-20 23:11:03 +0100

    feat: add edit and setting button and window

---
+++ client/testproject/src/components/IconButton.vue
@@ -0,0 +1,57 @@
+<script>
+export default{
+    props: {
+        message: String,
+        icon: { String, required: true },
+        fnc: { Function, required: true}
+    }
+}
+</script>
+
+<template>
+    <div class="container left-margin">
+        <button class="center" @click="fnc">
+            <img class="iconSize" :src="icon" alt="Image not found">
+            <p> {{ message }} </p>
+        </button>
+    </div>
+</template>
+
+<style>
+
+/* Invisible background
+    On hover darken.
+*/
+button{
+    width: 80px;
+    border: 0;
+    background-color: transparent;
+}
+
+button:hover{
+    background-color: silver;
+    cursor: pointer;
+}
+
+.container{
+    padding: 5px;
+}
+
+.left-margin:not(:first-of-type){
+    padding-left: 0;
+}
+
+.left-margin:first-of-type{
+    padding-right: 0;
+}
+
+.center{
+    position: relative;
+    margin: auto;
+}
+
+p {
+    font-size: 12px;
+}
+    
+</style>
---
+++ client/testproject/src/components/Window.vue
@@ -0,0 +1,67 @@
+<script>
+    export default {
+        props: {
+            title: String
+        },
+        data() {
+            return{
+                defaultPosition: {
+                    x: 200,
+                    y: 200
+                },
+
+                windowHover: false
+            }
+        },
+        methods: {
+            moveWindow(){
+                
+            }
+        },
+    }
+</script>
+
+<template>
+    <div 
+        class="window" 
+        :draggable="windowHover"
+        >
+        <div 
+            class="header" 
+            @mouseover="windowHover = true"
+            @mouseleave="windowHover = false"
+            :style="{ left: defaultPosition.x, top: defaultPosition.y}"
+            >
+            <div class="headerContainer">
+                {{ title }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+.headerContainer{
+    margin: 2px;
+}
+
+.header{
+    background-color: grey;
+    width: 100%;
+    height: 2em;
+    border-top-left-radius: 3px;
+    border-top-right-radius: 3px;
+}
+
+.header:hover{
+    cursor: grab;
+}
+
+.window{
+    border-radius: 3px;
+    background-color: azure;
+    width: 200px;
+    height: 200px;
+}
+
+</style>
