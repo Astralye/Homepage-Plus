3c795b0d9d8a65e6a98cf98a39a21068cfcb5cc3
Astralye <ainsleydatoon@gmail.com>
2024-06-21 18:10:24 +0100

    style: add window content and correct drag modifiers

--- client/testproject/src/components/MainButton.vue
+++ client/testproject/src/components/MainButton.vue
@@ -7,6 +7,9 @@ export default{
     components: {
         IconButton
     },
+    emits: [
+        'openWindowTab'
+    ],
     data() {
         return{
             editIcon: "editing.png",
--- client/testproject/src/components/Window.vue
+++ client/testproject/src/components/Window.vue
@@ -3,17 +3,29 @@
         props: {
             title: String
         },
+        emits: ['closeWindow'],
+
         data() {
             return{
                 defaultPosition: {
                     x: 200,
                     y: 200
                 },
-
                 windowHover: false
             }
         },
         methods: {
+
+            // startDrag(event){
+            //     console.log(event);
+            //     event.dataTransfer.dropEffect = 'move';
+            //     event.dataTransfer.effectAllowed = 'move';
+            // },
+
+            // onDrop(event){
+
+            // },
+            
             moveWindow(){
                 
             }
@@ -28,21 +40,72 @@
         >
         <div 
             class="header" 
-            @mouseover="windowHover = true"
-            @mouseleave="windowHover = false"
+            @mouseenter.self="windowHover = true"
+            @mouseleave.self="windowHover = false"
+            @dragstart="startDrag($event)"
             :style="{ left: defaultPosition.x, top: defaultPosition.y}"
             >
-            <div class="headerContainer">
-                {{ title }}
+            <div class="flex">
+                <div class="headerContent headerText">
+                    {{ title }}
+                </div>
+                <button 
+                    class="headerContent exitBtn noselect"
+                    @click="$emit('closeWindow', `${title}`)"
+                    @mouseenter.self="windowHover = false"
+                    >
+                    X
+                </button>
             </div>
+        </div>
+        <div
+        class="container noselect">
+
+            <!-- WindowContent, this would be a 
+                component using a forloop. -->
+            Hello world!
         </div>
     </div>
 </template>
 
 <style scoped>
+.container{
+    background-color: beige;
+    height: 100px;
+    
+    padding: 4px;
+    border-bottom-left-radius: 3px;
+    border-bottom-right-radius: 3px;
 
-.headerContainer{
-    margin: 2px;
+    color: black;
+
+    /* Todo:
+        Window width and height variable
+        -> Set shadow width + height
+        -> Set header width 
+    */
+    resize: both;
+    overflow: auto;
+}
+
+.headerContent{
+    margin-top: auto;
+    margin-bottom: auto;
+}
+
+.headerText{
+    margin-left: 4px;
+    font-size: 16px;
+}
+
+.exitBtn{
+    background-color: rgb(228, 69, 69);
+    margin-right: 4px;
+    color: white;
+    height: 25px;
+    width: 25px;
+    font-size: 12px;
+    border-radius: 5px;
 }
 
 .header{
@@ -53,15 +116,32 @@
     border-top-right-radius: 3px;
 }
 
+.flex{
+    display: flex;
+    flex-wrap: nowrap;
+    flex-direction: row;
+    height: 100%;
+    justify-content: space-between;
+}
+
 .header:hover{
     cursor: grab;
 }
 
 .window{
-    border-radius: 3px;
-    background-color: azure;
     width: 200px;
-    height: 200px;
+    height: auto;
+    box-shadow: 4px 4px 10px grey;
+    position: absolute;
 }
+
+/* .shadow{
+    position: absolute;
+    
+    width: 100px;
+    height: 100px;
+    background: black;
+    filter:blur(10px); 
+} */
 
 </style>
