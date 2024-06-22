5035e7175d1310309dd2b7be3d96906f53d8de74
Astralye <ainsleydatoon@gmail.com>
2024-06-23 00:04:01 +0100

    feat: add draggable window

--- client/testproject/src/components/Window.vue
+++ client/testproject/src/components/Window.vue
@@ -7,43 +7,60 @@
 
         data() {
             return{
-                defaultPosition: {
-                    x: 200,
-                    y: 200
-                },
-                windowHover: false
+                windowHover: false,
+
+                positions: {
+                    clientX: undefined,
+                    clientY: undefined,
+                    movementX: 0,
+                    movementY: 0
+                }
             }
         },
-        methods: {
+        methods: {          
+            dragMouseDown: function ( event ){
 
-            // startDrag(event){
-            //     console.log(event);
-            //     event.dataTransfer.dropEffect = 'move';
-            //     event.dataTransfer.effectAllowed = 'move';
-            // },
+                if(!this.windowHover) {return; }
+                event.preventDefault();
 
-            // onDrop(event){
+                // Get mouse location
+                this.positions.clientX = event.clientX;
+                this.positions.clientY = event.clientY;
 
-            // },
-            
-            moveWindow(){
-                
+                // register on mouse move and mouse up events
+                document.onmousemove = this.elementDrag;
+                document.onmouseup = this.closeDragElement;
+            },
+            elementDrag: function ( event) {
+                event.preventDefault();
+
+                this.positions.movementX = this.positions.clientX - event.clientX
+                this.positions.movementY = this.positions.clientY - event.clientY
+                this.positions.clientX = event.clientX
+                this.positions.clientY = event.clientY
+
+                this.$refs.draggableContainer.style.top = (this.$refs.draggableContainer.offsetTop - this.positions.movementY) + 'px'
+                this.$refs.draggableContainer.style.left = (this.$refs.draggableContainer.offsetLeft - this.positions.movementX) + 'px'
+            },
+            closeDragElement () {
+                document.onmouseup = null;
+                document.onmousemove = null;
             }
         },
     }
 </script>
 
 <template>
-    <div 
+    <div
+        ref="draggableContainer" 
         class="window" 
-        :draggable="windowHover"
         >
         <div 
             class="header" 
+            
+            @mousedown="dragMouseDown"
             @mouseenter.self="windowHover = true"
             @mouseleave.self="windowHover = false"
-            @dragstart="startDrag($event)"
-            :style="{ left: defaultPosition.x, top: defaultPosition.y}"
             >
             <div class="flex">
                 <div class="headerContent headerText">
@@ -66,9 +83,11 @@
             Hello world!
         </div>
     </div>
+    
 </template>
 
 <style scoped>
+
 .container{
     background-color: beige;
     height: 100px;
@@ -135,6 +154,15 @@
     position: absolute;
 }
 
+/* A box shadow is defined with container
+    offset, x + y
+    blur
+    color
+
+    By default css the size is the size of the container,
+    but we could try change the size of the container manually
+
+*/
 /* .shadow{
     position: absolute;
     
