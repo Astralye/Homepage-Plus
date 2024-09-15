a51b83ada454da4c614c55d7df269e814c8dcd67
Ainsley Pilongo <nmk21kfu@UEA.AC.UK>
2024-09-15 18:14:42 +0100

    feat: responsive grid gaps based on container size

--- client/testproject/src/components/Main/GridLayout.vue
+++ client/testproject/src/components/Main/GridLayout.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="fill grid base-margin border-box">
+    <div class="fill grid base-margin border-box"
+        ref="container"
+        >
         <template v-for="(item, index) in m_Rows * m_Columns" :key="index">
-            <div class="tmpbox"> {{index}} </div>
+            <div class="grid-item">
+                <div class="flex-center">
+                    {{ index }} 
+                </div>
+            </div>
         </template>
     </div>
 </template>
@@ -11,28 +17,42 @@ import { containerData } from '../../Data/containerData';
 
 export default {
     props:{
-        componentID: {
+        component_ID: {
             type: String,
             default: "",
             required: true
+        },
+        update_Grid_Flag: {
+            type: Boolean,
+            default: false,
         }
     },
     created(){
         this.m_containerData = this.getContainerData();
         this.setDimension();
+        this.m_iconSize = containerData.getIconSize(this.component_ID);
+    },
+    beforeUpdate(){
+        console.log(this.update_Grid_Flag)
     },
     data(){
         return{
             m_containerData: null,
+            
             m_Column_Data: "",
+            m_Column_Gap: "",
+
             m_Rows_Data: "",
+            m_Row_Gap: "",
 
             m_Rows: 0,
             m_Columns: 0, 
+
+            m_iconSize: 0,
         }
     },
     methods: {
-        getContainerData(){ return containerData.getObjectFromID(this.componentID); },
+        getContainerData(){ return containerData.getObjectFromID(this.component_ID); },
 
         // Dimension is stored as 'C,R'
         setRowColData(dimString){
@@ -45,12 +65,39 @@ export default {
         },
 
         setDimension(){
-            let dim = containerData.getGridDimension(this.componentID);
+            let dim = containerData.getGridDimension(this.component_ID);
             this.setRowColData(dim);
+            this.calculateGridGap();
+        },
+
+        /*
+            This calcualtes grid gap
+
+            There are bugs when there is only 1 row/ column as there is no grid gap.
+            Will need to take into consideration.
+        */
+
+        calculateGridGap(){
+            this.$nextTick( () => {
+                let data = this.$refs["container"].getBoundingClientRect();
+
+                let columnGap = (data.width  - (this.m_iconSize * this.m_Rows))    / ( this.m_Rows - 1 );
+                let rowGap    = (data.height - (this.m_iconSize * this.m_Columns)) / ( this.m_Columns - 1 );
+
+                this.m_Column_Gap = `${columnGap}px`;
+                this.m_Row_Gap    = `${rowGap}px`;
+            })
         },
     },
     watch: {
-        'm_containerData.gridData.gridDimensions'(){ this.setDimension(); }
+        'm_containerData.gridData.gridDimensions'(){ this.setDimension(); },
+        '$GlobalStates.value.edit.windowSize':{
+            handler(val,oldval){ this.calculateGridGap(); },
+            deep: true
+        },
+        'update_Grid_Flag'(){
+            this.calculateGridGap();
+        }
     }
 }
 </script>
@@ -60,10 +107,14 @@ export default {
     box-sizing: border-box;
 }
 
-.tmpbox{
-    width:  100%;
-    height: 100%;
-    background-color: goldenrod;
+.flex-center{
+    display: flex;
+    justify-content: center;
+    align-content: center;
+}
+
+.grid-item{
+    border: solid white 1px;
 }
 
 .fill{
@@ -76,8 +127,8 @@ export default {
     grid-template-columns: v-bind("m_Column_Data");
     grid-template-rows:    v-bind("m_Rows_Data");
 
-    grid-row-gap: 5px;
-    grid-column-gap: 5px;
+    grid-row-gap:    v-bind("m_Row_Gap");
+    grid-column-gap: v-bind("m_Column_Gap");
 
     /* 
     
@@ -89,8 +140,5 @@ export default {
     */
 }
 
-.base-margin{
-    padding: 0.5em;
-}
 
 </style>
