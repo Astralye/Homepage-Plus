c51c1f6bcdac41e28d870ee9f7d8fe4f31fd9ca1
Ainsley Pilongo <nmk21kfu@UEA.AC.UK>
2024-09-29 18:11:37 +0100

    feat: svg handler
    requires converting svgs into vue components over templates

---
+++ client/testproject/src/components/Input Components/SVGIcon.vue
@@ -0,0 +1,24 @@
+<script>
+import { defineAsyncComponent } from 'vue';
+
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+
+  computed: {
+    dynamicComponent() {
+      if(!this.name){ return null; }
+      const name = this.name;
+      return defineAsyncComponent(() => import(`../../assets/Icons/${name}.vue`));
+    },
+  },
+};
+</script>
+
+<template>
+    <component :is="dynamicComponent"/>
+</template>
