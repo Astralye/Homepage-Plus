69ecd3aa0c82b20a71aa365c53b11be39d4e0838
Ainsley Pilongo <nmk21kfu@UEA.AC.UK>
2024-10-07 21:19:39 +0100

    feat!: svg icon data only stores d rather than whole file
    BREAKING CHANGES: icons that used the old icon handler do not work

--- client/testproject/src/components/Input Components/SVGIcon.vue
+++ client/testproject/src/components/Input Components/SVGIcon.vue
@@ -1,3 +1,17 @@
+<!-- 
+  This file is responsible for loading the SVG
+
+  Currently, the SVG is saved as a template , with the svg content saved on it, only being uploaded as such.
+
+  To make the SVG a better component to react to size, and scale, the data (path), should be the only variable in the component.
+  This means I can make a SVG component, to automatically generate the SVG, given any width, height, colour, requirements.
+  The main important part of the component should be the 'path', which is how the SVG is drawn.
+
+  To make this easier for myself, I will make a new component using this 'SVGHandler', and delete this component later and adjust the current code,
+  and implementation.
+
+-->
+
 <script>
 import { defineAsyncComponent } from 'vue';
 
@@ -20,5 +34,5 @@ export default {
 </script>
 
 <template>
-    <component :is="dynamicComponent"/>
+    <!-- <component :is="dynamicComponent"/> -->
 </template>
---
+++ client/testproject/src/components/Input Components/SVGManip.vue
@@ -0,0 +1,28 @@
+<!-- 
+    This file is responsible in only loading the all the current SVG data
+
+
+-->
+
+<template>
+    <SVGHandler>
+        
+    </SVGHandler>
+</template>
+
+<script>
+import SVGHandler from './SVGHandler.vue';
+
+export default {
+    components: {
+        SVGHandler
+    },
+    props:{
+        data: String,
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
