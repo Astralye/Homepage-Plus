176cbe4970b01b607bb6963dfdcb99a118973364
Astralye <ainsleydatoon@gmail.com>
2024-11-09 20:41:18 +0000

    fix: allow nullable inputs and resetter

--- client/testproject/src/components/Main/IconHandler.vue
+++ client/testproject/src/components/Main/IconHandler.vue
@@ -8,20 +8,19 @@
         <div class="center fit-content">
 
             <SVGHandler
-                :height="icon_data.iconSize"
+                :height="iconData.size"
                 width="auto"
-                :path_Value="iconImageStorage.getPathData(icon_data.iconImage)"
-                :fill_Colour="icon_data.iconColour"
-                :view_Box="iconImageStorage.getViewBoxName(icon_data.iconImage)"
-
+                :path_Value="iconData.image"
+                :fill_Colour="iconData.colour"
+                :view_Box="iconData.viewBox"
                 @dblclick="(this.$GlobalStates.value.edit.enabled) ? null : openLink()"
             />
         </div>
 
-        <template v-if="icon_data.displayText">
+        <template v-if="iconData.displayText">
             <div class="center fit-content text-padding">
                 <p>
-                    {{ iconText }}
+                    {{ iconData.text }}
                 </p>
             </div>
         </template>
@@ -41,28 +40,44 @@ export default {
         icon_data: {
             type: Object,
             default: null,
-            required: true,
         },
     },
     data(){
         return{
             iconImageStorage,
 
-            iconText: ""
+            iconData: {
+                size: "10",
+                image: "",
+                colour: "",
+                viewBox: "10 10 10 10",
+                link: "",
+                text: "",
+                displayText : false,
+            }
         }
     },
     created(){
-        this.iconText = this.icon_data.iconString; 
+        this.init();
     },
     methods: {
+        init(){
+            if(!this.icon_data){ return; }
+            this.iconData.size    = this.icon_data.iconSize;
+            this.iconData.image   = iconImageStorage.getPathData(this.icon_data.iconImage);
+            this.iconData.colour  = this.icon_data.iconColour;
+            this.iconData.viewBox = iconImageStorage.getViewBoxName(this.icon_data.iconImage);
+            this.iconData.link    = this.icon_data.link;
+            this.iconData.text    = this.icon_data.text;
+            this.iconData.displayText = this.icon_data.displayText; 
+        },
         openLink(){
-            console.log(this.icon_data.link);
-            window.open(this.icon_data.link, '_blank');
+            window.open(this.iconData.link, '_blank');
         },
     },
     watch: {
         'icon_data':{
-            handler(val){ this.iconText = val.iconString },
+            handler(val){ this.init(); },
             deep: true            
         }
     },
