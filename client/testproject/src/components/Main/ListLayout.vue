a45175adc93ebf919aafa8fe55ae020704ce07d7
Astralye <ainsleydatoon@gmail.com>
2024-12-06 20:10:20 +0000

    feat(List): List item click JS implementation and CSS

--- client/testproject/src/components/Main/ListLayout.vue
+++ client/testproject/src/components/Main/ListLayout.vue
@@ -11,28 +11,42 @@
 
 <template>
     <!-- Container wrapper -->
-    <div class="height-full">
-        <div v-for="(item, index) in m_GroupData" :key="index"
-            class="list-item-wrapper flex">
-            
-            <!-- Icon if any -->
-            <div v-if="hasIcon(item)"
-                class="icon-spacing">
-                <SVGHandler
-                    class="icon-center"
-                    width="2em"
-                    height="2em"
-                    :path_Value="iconImageStorage.getPathData(item.iconImage)"
-                    :view_Box="iconImageStorage.getViewBoxName(item.iconImage)"
-                    :fill_Colour="item.iconColour"
-                />
-            </div>
+    <div class="height-full"
+        @click.self="iconSelect.resetData()"
+        @mouseup="checkDropIcon(index)"
+    >
+        <div v-for="(item, index) in m_GroupData" :key="index">
+            <div            
+                class="list-item-wrapper flex"
+                :class="{ 'icon-Selection' : isSelectedIcon(index),
+                        'unselect-icon'  : !isSelectedIcon(index)
+                }"
+                @click="(editVariables.isIconSelector) ? setSelectedIcon(index) : null"
+                >
+                    <!-- Icon if any -->
+                    <div v-if="hasIcon(item)"
+                        class="icon-spacing"
+                        >
+                        <SVGHandler
+                            class="icon-center"
+                            width="2em"
+                            height="2em"
+                            :path_Value="iconImageStorage.getPathData(item.iconImage)"
+                            :view_Box="iconImageStorage.getViewBoxName(item.iconImage)"
+                            :fill_Colour="item.iconColour"
+                        />
+                    </div>
+        
+                    <!-- Name -->
+                    <div
+                        class="center-text">
+                        {{ item.iconString }}
+                    </div>
 
-            <!-- Name -->
-            <div
-                class="center-text">
-                {{ item.iconString }}
-            </div>
+                </div>
+
+            <hr v-if="!isLastPosition(index)">
+
         </div>
     </div>
 </template>
@@ -40,7 +54,8 @@
 <script>
 
 import { containerData } from '../../Data/containerData';
-import { iconData } from '../../Data/iconData';
+import { iconData, iconSelect } from '../../Data/iconData';
+import { editVariables } from '../../Data/SettingVariables';
 
 import SVGHandler from '../Input Components/SVGHandler.vue';
 
@@ -61,9 +76,12 @@ export default {
         return{
             iconImageStorage,
             containerData,
+            editVariables,
+            iconSelect,
             iconData,
 
             m_GroupData: null,
+            m_containerData: null,
         }
     },
     created(){
@@ -83,14 +101,58 @@ export default {
             this.m_GroupData = iconData.getGroup(this.component_ID);
         },
 
-        hasIcon(item){
-            return (item.iconID !== "");
-        }
+        // If iconID is empty
+        hasIcon(item){ return (item.iconID !== ""); },
+
+        // checks if index is last position of the indexing.
+        isLastPosition(index){ return (this.m_GroupData.length-1 === index); },
+
+    // Taken from GridLayout.vue
+
+        // Click selection for linkmaker
+        isSelectedIcon(index){
+
+            let icon = this.m_GroupData[index];
+            if(!icon || !iconSelect) return false;
+            return (icon.iconID === iconSelect.data.iconID && this.m_containerData.ID === iconSelect.data.groupID);
+        },
+
+
+        // Because they are rendered in indexes, they must have values.
+        setSelectedIcon(index){
+
+            let icon = this.m_GroupData[index];
+            if(!icon) return; 
+            // no data, however, this should not occur because
+            // it indexes through each of the elements
+
+            iconSelect.setData(icon.iconID, this.m_containerData.ID);
+        },
     },
 }
 </script>
 
 <style scoped>
+
+.icon-Selection{
+    border: 3px solid rgba(255, 255, 255, 0.8);
+    border-radius: 10px;
+    
+    -webkit-transition: border-color 0.15s linear; /* Saf3.2+, Chrome */
+       -moz-transition: border-color 0.15s linear; /* FF3.7+ */
+         -o-transition: border-color 0.15s linear; /* Opera 10.5 */
+            transition: border-color 0.15s linear;
+}
+
+.unselect-icon{
+    border: 3px solid rgba(255, 255, 255, 0.1);
+    border-radius: 10px;
+
+    -webkit-transition: border-color 0.15s linear; /* Saf3.2+, Chrome */
+       -moz-transition: border-color 0.15s linear; /* FF3.7+ */
+         -o-transition: border-color 0.15s linear; /* Opera 10.5 */
+            transition: border-color 0.15s linear;
+}
 
 .icon-spacing{
     padding-right: 0.75em;
@@ -114,10 +176,6 @@ export default {
     height: fit-content;
 
     padding: 0.5em;
-}
-
-.list-item-wrapper:not(:last-child){
-    border-bottom: var(--Accent-background-colour) solid 2px;
 }
 
 .height-full{
